04433ec2bca8ff48191a60430317466c0e2c7340
shh4und <alexandersznunes@gmail.com>
2024-06-28 20:59:51 -0300

    using bilateral and erosion instead of median

--- OP_1.py
+++ OP_1.py
@@ -1,11 +1,13 @@
-
 from ip.ios import *
 from ip.enhancement import *
 from ip.binary import *
 from ip.graph_nx import *
 from ip.swc import *
 from ip.split import *
+from ip.utils import *
 from skimage.morphology import skeletonize
+from skimage.util import img_as_ubyte
+from scipy import ndimage
 
 # ## Read Data
 
@@ -15,14 +17,20 @@ images = load_image_stack(folder_path)
 
 # ## Code Process and Execution
 
-threshold1 = mean_threshold(images, 15)
-binary1 = simple_binary(images, threshold1)
-seg = segment(images, binary1)
-denoising = median_blur(seg, 5)
-threshold2 = mean_threshold(denoising, 15)
-binary2 = simple_binary(denoising, threshold2)
+bilateral = bilateral_blur(images, 9, 175, 175)
 
-skel = skeletonize(binary2)
+threshold1 = mean_threshold(bilateral)
+binary1 = simple_binary(bilateral, threshold1)
+seg1 = segment(bilateral, binary1)
+
+threshold2 = mean_threshold(seg1)
+binary2 = simple_binary(seg1, threshold2)
+
+strel3 = create_spherical_strel(3)
+eroded3 = ndimage.binary_erosion(binary2, strel3)
+
+
+skel_op3 = img_as_ubyte(skeletonize(eroded3))
 # ### For visualization purposes only
 
 # blend = blended(skel)
@@ -30,12 +38,12 @@ skel = skeletonize(binary2)
 
 # ### Graph generation
 
-graph = Graph(skel)
-graph.set_root((2, 428, 33))
+graph = Graph(skel_op3)
+graph.set_root((3, 429, 34))
 graph.create_graph()
-root = list(graph.get_root())[::-1]
+root = graph.get_root()
 g_root = (30.979,429.04,0)
-print(f"OP_1 GOLD STANDARD ROOT: {g_root}\nTEST ROOT: {tuple(root)}")
+print(f"OP_1 GOLD STANDARD ROOT: {g_root}\nTEST ROOT: {root}")
 
 mst = graph.apply_dfs_and_label_nodes()
 
