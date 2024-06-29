dfd8d1377e7eb549326fc05f163fd6f26993198a
shh4und <alexandersznunes@gmail.com>
2024-06-28 21:00:22 -0300

    using bilateral filtering instead of flood fill

--- OP_2.py
+++ OP_2.py
@@ -4,41 +4,36 @@ from ip.binary import *
 from ip.graph_nx import *
 from ip.swc import *
 from ip.split import *
-from skimage.morphology import skeletonize, flood_fill
+from ip.utils import *
+from skimage.morphology import skeletonize
+from skimage.util import img_as_ubyte
+from scipy import ndimage
 
 # ## Read Data
 
 folder_path = "./OlfactoryProjectionFibers/ImageStacks/OP_2"
-
 images = load_image_stack(folder_path)
 
-# ## Code Process and Execution
+bilateral = bilateral_blur(images, 11, 200, 200)
+# bilateral = bilateral_blur(images, 9, 200, 200) Score: 0,781
 
-floodf = flood_fill(images, (40, 267, 470), 0, tolerance=235)
-floodf2 = flood_fill(floodf, (48, 282, 64), 0, tolerance=90)
-floodf3 = flood_fill(floodf2, (43, 251, 105), 0, tolerance=200)
-floodf4 = flood_fill(floodf3, (0, 367, 52), 0, tolerance=35)
+threshold1 = mean_threshold(bilateral, 50)
+binary1 = simple_binary(bilateral, threshold1)
+seg1 = segment(bilateral, binary1)
 
-split_flood = split_and_process_stack(floodf4, 250, 5)
-
-skel = skeletonize(split_flood)
-
-# ### For visualization purposes only
-
-blend = blended(skel)
-single_download(blend, "./Test/Images/OP_2_skel.png")
-
+threshold2 = mean_threshold(seg1, 50)
+binary2 = simple_binary(seg1, threshold2)
 # ### Graph generation
+
+skel = img_as_ubyte(skeletonize(binary2))
 
 graph = Graph(skel)
 graph.set_root((26, 391, 1))
 graph.create_graph()
-root = graph.get_root()
+root = list(graph.get_root())[::-1]
 g_root = (0.72501,391.08,25)
-print(f"OP_2 GOLD STANDARD ROOT: {g_root}\nTEST ROOT: {root}")
+print(f"OP_1 GOLD STANDARD ROOT: {g_root}\nTEST ROOT: {tuple(root)}")
 
-distances, paths = graph.apply_dijkstra_and_label_nodes()
+mst = graph.apply_dfs_and_label_nodes()
 
-print(f"from dijkstra -> len(distances): {len(distances)}, len(paths): {len(paths)}")
-
-print("SWC generated:",graph.save_to_swc("./Test/OP_2.swc"))
+graph.save_to_swc("./Test/OP_2.swc")
