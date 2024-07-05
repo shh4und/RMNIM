f9d7d6de8815f2bec150fa0f5eebc31872817cf9
shh4und <alexandersznunes@gmail.com>
2024-07-04 22:27:05 -0300

    better descriptions

--- ip/binary.py
+++ ip/binary.py
@@ -1,43 +1,96 @@
 import cv2
 import numpy as np
-from skimage.morphology import thin
-from skimage import img_as_ubyte
-from concurrent.futures import ThreadPoolExecutor
 
-def segment(images, binaries):
-    return np.array([cv2.bitwise_and(img, img, mask=bin) for img, bin in zip(images, binaries)])
+def segment(images: np.ndarray, mask: np.ndarray) -> np.ndarray:
+    """Function to make a image stack segmentation with its mask
 
-def otsu_binary(images):
+    Args:
+        images (NDArray): image stack
+        mask (NDArray): image stack mask
+
+    Returns:
+        NDArray: returns a segmented image stack
+    """
+    return np.array([cv2.bitwise_and(img, img, mask=binary) for img, binary in zip(images, mask)])
+
+def otsu_binary(images: np.ndarray) -> np.ndarray:
+    """Function to create a binary image stack with Otsu threshold technique
+
+    Args:
+        images (NDArray): image stack
+
+    Returns:
+        NDArray: returns a Otsu binary mask of the given image stack
+    """
     return np.array([cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1] for img in images])
 
-def simple_binary(images, n):
-    return np.array([cv2.threshold(img, n, 255, cv2.THRESH_BINARY)[1] for img in images])
+def simple_binary(images: np.ndarray, thresh: int) -> np.ndarray:
+    """Function to create a binary image stack from a chosen bottom threshold 
 
-def morphological(img, op, shape_kernel, size_kernel, iter_num=None, kernel=None):
-    if kernel is None:
-        kernel = cv2.getStructuringElement(shape_kernel, size_kernel)
-        if iter_num is None:
-            return cv2.morphologyEx(img, op, kernel)
-        else:
-            return cv2.morphologyEx(img, op, kernel, iterations=iter_num)
-    else:
-        return cv2.morphologyEx(img, op, kernel)
+    Args:
+        images (NDArray): image stack
+        thresh (int): threshold value
 
-def thinned(images, iter=1):
-    return np.array([img_as_ubyte(thin(img, iter)) for img in images])
+    Returns:
+        NDArray: returns a binary mask of the given image stack
+    """
+    return np.array(images > thresh).astype(images.dtype)
 
-def skel_zhang_suen(images):
+def morphological(images: np.ndarray, op, shape_kernel, size_kernel: tuple[int]) -> np.ndarray:
+    """Function to apply morphological operation from OpenCV lib over a given image stack
+
+    Args:
+        images (NDArray): image stack
+        op (MorphTypes): type of a morphological operation such as: cv.MORPH_ERODE, cv.MORPH_DILATE, cv.MORPH_OPEN, cv.MORPH_CLOSE
+        shape_kernel (MorphShapes): element shape that could be one of cv.MORPH_RECT, cv.MORPH_CROSS or cv.MORPH_ELLIPSE
+        size_kernel (tuple(int, int)): size of the structuring element
+
+    Returns:
+        NDArray: returns a image stack from a chosen morphological operation
+    """
+
+    kernel = cv2.getStructuringElement(shape_kernel, size_kernel)
+    
+    return np.array([cv2.morphologyEx(img, op, kernel) for img in images])
+    
+    
+
+
+def skel_zhang_suen(images: np.ndarray) -> np.ndarray:
+    """Function to compute a skeleton of a given image stack using thinning technique of Zhang-Suen (from OpenCV lib).
+
+    Args:
+        images (NDArray): image stack
+
+    Returns:
+        NDArray: returns a skeleton 
+    """
     return np.array([cv2.ximgproc.thinning(img) for img in images])
 
-def mean_threshold(img, factor=1):
-    return np.ceil((img.sum() / img.size) * factor)
+def mean_threshold(images: np.ndarray, factor:int = 1) -> int:
+    """Function to calculate the mean value of a given image stack
 
-# Exemplo de uso com paralelização para a função otsu_binary
-def otsu_binary_parallel(images, num_threads=4):
-    with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        return np.array(list(executor.map(otsu_binary, images)))
+    Args:
+        images (NDArray): image stack
+        factor (int, optional): optional factor to multiply the mean value. Defaults to 1.
 
-# Exemplo de uso com paralelização para a função simple_binary
-def simple_binary_parallel(images, n, num_threads=4):
-    with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        return np.array(list(executor.map(lambda img: simple_binary([img], n)[0], images)))
+    Returns:
+        int: returns a ceiled value of the mean
+    """
+    return np.ceil((images.sum() / images.size) * factor)
+
+
+## For testing
+
+# def thinned(images, iter=1):
+#     return np.array([img_as_ubyte(thin(img, iter)) for img in images])
+
+# # Exemplo de uso com paralelização para a função otsu_binary
+# def otsu_binary_parallel(images, num_threads=4):
+#     with ThreadPoolExecutor(max_workers=num_threads) as executor:
+#         return np.array(list(executor.map(otsu_binary, images)))
+
+# # Exemplo de uso com paralelização para a função simple_binary
+# def simple_binary_parallel(images, n, num_threads=4):
+#     with ThreadPoolExecutor(max_workers=num_threads) as executor:
+#         return np.array(list(executor.map(lambda img: simple_binary([img], n)[0], images)))
--- ip/utils.py
+++ ip/utils.py
@@ -1,42 +1,61 @@
 import numpy as np
 
-def normalize_image_int(image):
+def normalize_image_int(image: np.ndarray) -> np.ndarray:
+    """Function to normalize a given image from float type range (0,1) to uint type range (0,255)
+
+    Args:
+        image (np.ndarray): given image
+
+    Returns:
+        np.ndarray: returns a normalized uint image
+    """
     np_image = np.array(image).astype('float32')
     min_val = np.min(np_image)
     max_val = np.max(np_image)
     
-    # Normalizar para a faixa de 0 a 255
     np_image_normalized = 255 * (np_image - min_val) / (max_val - min_val)
     
     return np_image_normalized.astype('uint8')
 
-def normalize_image_float(image):
+def normalize_image_float(image: np.ndarray) -> np.ndarray:
+    """Function to normalize a given image from uint type range (0,255) to float type range (0,1)
+
+    Args:
+        image (np.ndarray): given image
+
+    Returns:
+        np.ndarray: returns a normalized float image
+    """
     np_image = np.array(image).astype('float32') / 255.0
     return np_image
 
-def create_spherical_strel(size):
-    # Certifique-se de que o tamanho é ímpar para que haja um centro definido
-    assert size % 2 == 1, "O tamanho deve ser um número ímpar para ter um centro definido."
+def create_spherical_strel(size: int) -> np.ndarray:
+    """Function to create a spherical structuring element
+    the size must be an odd number so that there is a defined center
 
-    # Criar um array 3D de zeros
+    Args:
+        size (odd int): size of the strel
+
+    Returns:
+        np.ndarray: returns a spherical structuring element of the given size
+    """
+    # Ensure the size is odd so that there is a defined center
+    assert size % 2 == 1, "Size must be an odd number to have a defined center."
+
+    # Create a 3D array of zeros
     strel = np.zeros((size, size, size), dtype=int)
 
-    # Definir o centro
+    # Define the center
     center = size // 2
     radius = center
 
-    # Preencher a esfera
+    # Fill the sphere
     for x in range(size):
         for y in range(size):
             for z in range(size):
-                # Calcular a distância do ponto (x, y, z) ao centro
+                # Calculate the distance from point (x, y, z) to the center
                 distance = np.sqrt((x - center) ** 2 + (y - center) ** 2 + (z - center) ** 2)
                 if distance <= radius:
                     strel[x, y, z] = 1
 
     return strel
-
-# # Exemplo de uso:
-# size = 3
-# spherical_strel = create_spherical_strel(size)
-# print(spherical_strel)
