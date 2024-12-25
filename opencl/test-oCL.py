de86f8b95351d86672f58fe9994f39b43ddebb30
shh4und <alexandersznunes@gmail.com>
2024-12-24 23:28:36 -0300

    nothing imporntant

--- opencl/test-oCL.py
+++
@@ -1,9 +0,0 @@
-import pyopencl as cl
-import pyopencl.array
-
-platforms = cl.get_platforms()
-for platform in platforms:
-    print("Platform:", platform.name)
-    devices = platform.get_devices()
-    for device in devices:
-        print("Device:", device.name)
